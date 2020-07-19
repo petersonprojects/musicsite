3c04f00983c3155c0bcc50a28e0f471abf7374f0
Micah Peterson <micahpeterson@live.com>
2020-07-19 18:31:03 -0500

    starting index page

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,36 @@ let dataFile = require('../data/data.json')
 
 router.get('/',(req,res)=>{
     //code and then res.render
+    let albums = dataFile.albums;
+
+
+    let artist = albums[0].name;
+
+    let albumCovers = []; //populate with all of the artwork
+    let albumNames = [];
+    let hrefsAM = [];
+    
+
+    albums.forEach(albumObj=>{
+        albumCovers = albumCovers.concat(albumObj.artwork[0])
+    })
+
+    albums.forEach(albumObj=>{
+        albumNames = albumNames.concat(albumObj.albumname)
+    })
+
+    albums.forEach(albumObj=>{
+        hrefsAM = hrefsAM.concat(albumObj.appleMusic)
+    })
+
+    res.render('index',{
+        albums:albums,
+        artwork: albumCovers,
+        albumnames: albumNames,
+        artist: artist,
+        hrefsAM: hrefsAM
+    })
+
 })
 
 module.exports = router;
